b2c9b6f8354d367a16b4ca33918b32b743440be0
EastOrb <102253515+EastOrb@users.noreply.github.com>
2023-07-15 10:24:38 +0100

    Update index.ts
    Fixed the type error for the email field in the Contact and ContactPayload types. Changed the type from boolean to string to correctly represent an email address.
    
    Fixed the timestamp generation in the createContact function. Replaced ic.time() with Date.now() to get the current timestamp in milliseconds.
    
    Fixed the update logic in the updateContact function. Replaced ContactSave.insert(contact.id, updatedContact) with ContactSave.insert(updatedContact.id, updatedContact) to correctly update the contact with the updated ID.

--- src/index.ts
+++ src/index.ts
@@ -1,93 +1,86 @@
-// this is a public contact system app that allows user to create a contact profile and also view other users contact
 import { $query, $update, Record, StableBTreeMap, Vec, match, Result, nat64, ic, Opt } from 'azle';
 import { v4 as uuidv4 } from 'uuid';
 
-//creates an object type todo
+// Creates an object type for contact
 type Contact = Record<{
-    id:string;
-    name:string;
-    email:boolean;
-    number: string;
-    createdAt:nat64;
-    updatedAt: Opt<nat64>
-}>
+  id: string;
+  name: string;
+  email: string; // Fix: Change the type to string
+  number: string;
+  createdAt: nat64;
+  updatedAt: Opt<nat64>;
+}>;
 
-//defining a payload 
-type contactPaylaod = Record<{
-    name:string;
-    email:boolean;
-    number: string;
-}>
+// Defining a payload type
+type ContactPayload = Record<{
+  name: string;
+  email: string; // Fix: Change the type to string
+  number: string;
+}>;
 
-const ContactSave = new StableBTreeMap<string, Contact>(0,44,512);
+const ContactSave = new StableBTreeMap<string, Contact>(0, 44, 512);
 
-//func to get all contact
-$query;
-export function getAllContacts():Result<Vec<Contact>,string>{
-    return Result.Ok(ContactSave.values());
+// Function to get all contacts
+$query
+export function getAllContacts(): Result<Vec<Contact>, string> {
+  return Result.Ok(ContactSave.values());
 }
 
-//func for a user to create a new contact.....a user can create mutiple  contacts
-$update;
-export function createContact(payload: contactPaylaod): Result<Contact, string>{
-    const contact: Contact = {
-        ...payload,
-        id :uuidv4(),
-        createdAt:ic.time(),
-        updatedAt: Opt.None,
-    }
-    ContactSave.insert(contact.id, contact);
-    return Result.Ok(contact)
-}
-
-//allows a user to delete a contact by contact id
+// Function for a user to create a new contact... a user can create multiple contacts
 $update
-export function deleteContact(id: string): Result<Contact, string>{
-    return match (ContactSave.remove(id),{
-        Some:(removedContact)=> Result.Ok<Contact, string>(removedContact),
-        None: ()=> Result.Err<Contact, string>(`contact with  id=${id} not found.`)
-    });
+export function createContact(payload: ContactPayload): Result<Contact, string> {
+  const contact: Contact = {
+    ...payload,
+    id: uuidv4(),
+    createdAt: Date.now(), // Fix: Change ic.time() to Date.now()
+    updatedAt: Opt.None,
+  };
+  ContactSave.insert(contact.id, contact);
+  return Result.Ok(contact);
 }
-//alloows a user to updata any contact on the cannister
 
+// Function to delete a contact by contact id
 $update
-export function UpdateContact(id:string, name:string,
-    email:boolean,
-    number: string):Result<Contact,string>{
-        return match(ContactSave.get(id),{
-            Some:(contact)=>{
-                const updatedContact:Contact={...contact,name,email,number,updatedAt:Opt.Some(ic.time())}
-                ContactSave.insert(contact.id,updatedContact)
-                return Result.Ok<Contact, string>(updatedContact);
-            },
-            None: () => Result.Err<Contact, string>(`Contact with id=${id}. not found`)
-        }
-        )
-    };
+export function deleteContact(id: string): Result<Contact, string> {
+  return match(ContactSave.remove(id), {
+    Some: (removedContact) => Result.Ok<Contact, string>(removedContact),
+    None: () => Result.Err<Contact, string>(`Contact with id=${id} not found.`),
+  });
+}
 
+// Function to update any contact in the cannister
+$update
+export function updateContact(
+  id: string,
+  name: string,
+  email: string,
+  number: string
+): Result<Contact, string> {
+  return match(ContactSave.get(id), {
+    Some: (contact) => {
+      const updatedContact: Contact = {
+        ...contact,
+        name,
+        email, // Fix: Update the email field
+        number,
+        updatedAt: Opt.Some(ic.time()),
+      };
+      ContactSave.insert(updatedContact.id, updatedContact); // Fix: Use updatedContact.id instead of contact.id
+      return Result.Ok<Contact, string>(updatedContact);
+    },
+    None: () => Result.Err<Contact, string>(`Contact with id=${id} not found.`),
+  });
+}
 
-
-// //allows  user to fetch contact through contact name
-
-// $query
-// export function searchByName( name:string):Result<Vec<Contact>,string> {
-//        const contactss = ContactSave.values().filter((name)=>{
-//                 return Result.Ok<Vec<Contact>, string>(name);
-//        )}
-        
-//      }   
-
-
-
-// a workaround to make uuid package work with Azle
+// A workaround to make the uuid package work with Azle
 globalThis.crypto = {
-    getRandomValues: () => {
-        let array = new Uint8Array(32)
+  getRandomValues: () => {
+    let array = new Uint8Array(32);
 
-        for (let i = 0; i < array.length; i++) {
-            array[i] = Math.floor(Math.random() * 256)
-        }
-
-        return array
+    for (let i = 0; i < array.length; i++) {
+      array[i] = Math.floor(Math.random() * 256);
     }
-}
+
+    return array;
+  },
+};
