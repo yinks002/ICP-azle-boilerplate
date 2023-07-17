57ba2161b0836c11058c7e8c0b4839eb73f72e93
Muhindo Galien <74584848+Muhindo-Galien@users.noreply.github.com>
2023-07-17 17:09:58 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,90 +4,101 @@ import { v4 as uuidv4 } from 'uuid';
 
 //creates an object type todo
 type Contact = Record<{
-    id:string;
-    name:string;
-    email:boolean;
+    id: string;
+    name: string;
+    email: boolean;
     number: string;
-    createdAt:nat64;
-    updatedAt: Opt<nat64>
-}>
+    createdAt: nat64;
+    updatedAt: Opt<nat64>;
+}>;
 
 //defining a payload 
-type contactPaylaod = Record<{
-    name:string;
-    email:boolean;
+type ContactPayload = Record<{
+    name: string;
+    email: boolean;
     number: string;
-}>
+}>;
 
-const ContactSave = new StableBTreeMap<string, Contact>(0,44,512);
+const ContactSave = new StableBTreeMap<string, Contact>(0, 44, 512);
 
-//func to get all contact
-$query;
-export function getAllContacts():Result<Vec<Contact>,string>{
+$query
+export function getAllContacts(): Result<Vec<Contact>, string> {
     return Result.Ok(ContactSave.values());
 }
 
-//func for a user to create a new contact.....a user can create mutiple  contacts
-$update;
-export function createContact(payload: contactPaylaod): Result<Contact, string>{
+//func to get all contact
+$update
+export function createContact(payload: ContactPayload): Result<Contact, string> {
+    // Input validation
+    if (!payload.name || payload.name.trim() === '') {
+        return Result.Err('Name is required.');
+    }
+
+    if (!payload.number || payload.number.trim() === '') {
+        return Result.Err('Number is required.');
+    }
+
+    // Check for duplicate contacts
+    const existingContact = ContactSave.values().find(contact => contact.name === payload.name);
+    if (existingContact) {
+        return Result.Err('A contact with the same name already exists.');
+    }
+
     const contact: Contact = {
         ...payload,
-        id :uuidv4(),
-        createdAt:ic.time(),
+        id: uuidv4(),
+        createdAt: ic.time(),
         updatedAt: Opt.None,
-    }
+    };
+
     ContactSave.insert(contact.id, contact);
-    return Result.Ok(contact)
+    return Result.Ok(contact);
+}
+
+//func for a user to create a new contact.....a user can create mutiple  contacts
+$update
+export function deleteContact(id: string): Result<Contact, string> {
+    const contact = ContactSave.get(id);
+    if (contact) {
+        ContactSave.remove(id);
+        return Result.Ok(contact);
+    } else {
+        return Result.Err(`Contact with ID ${id} not found.`);
+    }
 }
 
 //allows a user to delete a contact by contact id
 $update
-export function deleteContact(id: string): Result<Contact, string>{
-    return match (ContactSave.remove(id),{
-        Some:(removedContact)=> Result.Ok<Contact, string>(removedContact),
-        None: ()=> Result.Err<Contact, string>(`contact with  id=${id} not found.`)
-    });
-}
-//alloows a user to updata any contact on the cannister
-
-$update
-export function UpdateContact(id:string, name:string,
-    email:boolean,
-    number: string):Result<Contact,string>{
-        return match(ContactSave.get(id),{
-            Some:(contact)=>{
-                const updatedContact:Contact={...contact,name,email,number,updatedAt:Opt.Some(ic.time())}
-                ContactSave.insert(contact.id,updatedContact)
-                return Result.Ok<Contact, string>(updatedContact);
-            },
-            None: () => Result.Err<Contact, string>(`Contact with id=${id}. not found`)
-        }
-        )
-    };
-
-
-
-// //allows  user to fetch contact through contact name
-
-// $query
-// export function searchByName( name:string):Result<Vec<Contact>,string> {
-//        const contactss = ContactSave.values().filter((name)=>{
-//                 return Result.Ok<Vec<Contact>, string>(name);
-//        )}
-        
-//      }   
-
-
-
-// a workaround to make uuid package work with Azle
-globalThis.crypto = {
-    getRandomValues: () => {
-        let array = new Uint8Array(32)
-
-        for (let i = 0; i < array.length; i++) {
-            array[i] = Math.floor(Math.random() * 256)
-        }
-
-        return array
+export function updateContact(
+    id: string,
+    name: string,
+    email: boolean,
+    number: string
+): Result<Contact, string> {
+    const contact = ContactSave.get(id);
+    if (contact) {
+        const updatedContact: Contact = {
+            ...contact,
+            name,
+            email,
+            number,
+            updatedAt: Opt.Some(ic.time()),
+        };
+        ContactSave.insert(contact.id, updatedContact);
+        return Result.Ok(updatedContact);
+    } else {
+        return Result.Err(`Contact with ID ${id} not found.`);
     }
 }
+
+globalThis.crypto = {
+    getRandomValues: () => {
+        let array = new Uint8Array(32);
+
+        for (let i = 0; i < array.length; i++) {
+            array[i] = Math.floor(Math.random() * 256);
+        }
+
+        return array;
+    },
+};
